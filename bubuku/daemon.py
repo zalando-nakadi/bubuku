66c34dea5ab64b57c4135b9cd540f3681b86c545
Vyacheslav Stepanov <vyacheslav.stepanov@zalando.de>
2016-08-05 13:46:44 +0200

    fixed problem brought by refactoring

--- bubuku/daemon.py
+++ bubuku/daemon.py
@@ -31,9 +31,9 @@ def apply_features(features: str, controller: Controller, buku_proxy: BukuExhibi
         elif feature == 'rebalance_on_brokers_change':
             controller.add_check(RebalanceOnBrokerListChange(buku_proxy, broker))
         elif feature == 'rebalance_by_size':
-            controller.add_check(GenerateDataSizeStatistics(exhibitor, broker, CmdHelper(),
+            controller.add_check(GenerateDataSizeStatistics(buku_proxy, broker, CmdHelper(),
                                                             kafka_properties.get_property("log.dirs").split(",")))
-            controller.add_check(RebalanceBySize(exhibitor, broker))
+            controller.add_check(RebalanceBySize(buku_proxy, broker))
         elif feature == 'graceful_terminate':
             register_terminate_on_interrupt(controller, broker)
         elif feature == 'use_ip_address':
