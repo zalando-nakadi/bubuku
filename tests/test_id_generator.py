182693ab59c629a38ef20a19230e67dc2e5fe4e0
antban <dmitry.sorokin@gmail.com>
2016-08-09 14:04:09 +0200

    Fix max reserved id for kafka

--- tests/test_id_generator.py
+++ tests/test_id_generator.py
@@ -2,10 +2,29 @@ from bubuku.id_generator import BrokerIDByIp
 from test_config import build_test_properties
 
 
-def test_max_id_replaced_on_ip():
+def _test_max_id_replaced_on_ip(ip):
     kafka_properties = build_test_properties()
     assert kafka_properties.get_property('reserved.broker.max.id') is None
-    BrokerIDByIp(None, '192.168.1.1', kafka_properties)
+    gen = BrokerIDByIp(None, ip, kafka_properties)
 
+    id_ = int(gen.get_broker_id())
+    max_id = int(kafka_properties.get_property('reserved.broker.max.id'))
     # check that it's not negative and it can hold all the information
-    assert 3 * 256 * 256 * 256 <= int(kafka_properties.get_property('reserved.broker.max.id')) < 127 * 256 * 256 * 256
+    assert 3 * 256 * 256 * 256 <= max_id < 127 * 256 * 256 * 256
+
+    assert id_ < max_id
+
+
+def test_max_id_block_1():
+    _test_max_id_replaced_on_ip('10.0.0.0')
+    _test_max_id_replaced_on_ip('10.255.255.255')
+
+
+def test_max_id_block_2():
+    _test_max_id_replaced_on_ip('192.168.0.0')
+    _test_max_id_replaced_on_ip('192.168.255.255')
+
+
+def test_max_id_block_3():
+    _test_max_id_replaced_on_ip('172.16.0.0')
+    _test_max_id_replaced_on_ip('172.31.255.255')
