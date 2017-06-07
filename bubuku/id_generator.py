d479594761abbbb0199f801f9873c2b28c9d9856
Andrey Dyachkov <andrey.dyachkov@zalando.de>
2017-06-07 13:48:16 +0200

    aruha-729: removed parent class since there is no need

--- bubuku/id_generator.py
+++ bubuku/id_generator.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-import functools
 import logging
-import os
 import re
-from time import sleep, time
+
+import os
 
 from bubuku.config import KafkaProperties
 from bubuku.zookeeper import BukuExhibitor
@@ -12,29 +11,6 @@ _LOG = logging.getLogger('bubuku.id_generator')
 
 
 class BrokerIdGenerator(object):
-    def get_broker_id(self) -> str:
-        raise NotImplementedError('Not implemented')
-
-    def detect_broker_id(self):
-        raise NotImplementedError('Not implemented')
-
-    def wait_for_broker_id_absence(self):
-        while self.is_registered():
-            sleep(1)
-
-    def wait_for_broker_id_presence(self, timeout) -> bool:
-        start = time()
-        while not self.is_registered():
-            if (time() - start) > timeout:
-                return False
-            sleep(1)
-        return True
-
-    def is_registered(self):
-        raise NotImplementedError('Not implemented')
-
-
-class BrokerIdAutoAssign(BrokerIdGenerator):
     def __init__(self, zk: BukuExhibitor, kafka_properties: KafkaProperties):
         super().__init__()
         self.zk = zk
@@ -42,11 +18,9 @@ class BrokerIdAutoAssign(BrokerIdGenerator):
         self.broker_id = None
 
     def get_broker_id(self):
-        if not  self.broker_id:
-            self.broker_id = self.detect_broker_id()
-        return self.broker_id
+        if self.broker_id:
+            return self.broker_id
 
-    def detect_broker_id(self):
         meta_path = '{}/meta.properties'.format(self.kafka_properties.get_property('log.dirs'))
         while not os.path.isfile(meta_path):
             return None
@@ -55,11 +29,12 @@ class BrokerIdAutoAssign(BrokerIdGenerator):
             for line in lines:
                 match = re.search('broker\.id=(\d+)', line)
                 if match:
-                    return match.group(1)
+                    self.broker_id = match.group(1)
+                    return self.broker_id
         return None
 
     def is_registered(self):
-        broker_id = self.detect_broker_id()
+        broker_id = self.get_broker_id()
         if broker_id:
             return self.zk.is_broker_registered(broker_id)
         return False
