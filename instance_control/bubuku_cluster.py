d0d8da73a6c27dd5304976ff04dbcc55e8cb5a7e
Andrey Dyachkov <andrey.dyachkov@zalando.de>
2017-06-22 10:59:24 +0200

    bugfix: force option for upgrade

--- instance_control/bubuku_cluster.py
+++ instance_control/bubuku_cluster.py
@@ -25,8 +25,9 @@ def cli():
 @click.option('--ip', required=True)
 @click.option('--user', required=True)
 @click.option('--odd', required=True)
-def upgrade(image_version: str, cluster_config: str, ip: str, user: str, odd: str):
-    UpgradeCommand(cluster_config, image_version, ip, user, odd).run()
+@click.option('--force', type=bool, default=False)
+def upgrade(image_version: str, cluster_config: str, ip: str, user: str, odd: str, force: bool):
+    UpgradeCommand(cluster_config, image_version, ip, user, odd, force).run()
 
 
 @cli.command('attach', help='Launch instance and attach it to the existing EBS volume')
--- instance_control/command/upgrade.py
+++ instance_control/command/upgrade.py
@@ -14,12 +14,13 @@ _LOG = logging.getLogger('bubuku.cluster.command.upgrade')
 
 
 class UpgradeCommand(Command):
-    def __init__(self, cluster_config_path: str, image_version: str, ip: str, user: str, odd: str):
+    def __init__(self, cluster_config_path: str, image_version: str, ip: str, user: str, odd: str, force: bool):
         super().__init__(cluster_config_path)
         self.image_version = image_version
         self.ip = ip
         self.user = user
         self.odd = odd
+        self.force = force
 
     def alter_config(self):
         if self.image_version:
@@ -30,7 +31,8 @@ class UpgradeCommand(Command):
         ec2_resource = boto3.resource('ec2', region_name=self.cluster_config['region'])
 
         instance = node.get_instance_by_ip(ec2_resource, self.cluster_config['cluster_name'], self.ip)
-        check_current_image_version(instance, self.cluster_config['image_version'])
+        if not self.force:
+            check_current_image_version(instance, self.cluster_config['image_version'])
 
         _LOG.info('Searching for instance %s volumes', instance.instance_id)
         volumes = ec2_client.describe_instance_attribute(InstanceId=instance.instance_id,
