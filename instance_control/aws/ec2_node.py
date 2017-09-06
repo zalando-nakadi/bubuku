50d3c581c69c1bd52693f2249ccccf0458eaa0c7
dsorokin <dmitry.sorokin@gmail.com>
2017-09-06 10:10:58 +0200

    ARUHA-1099 Optimize "Describe instances" request, log each aws request result

--- instance_control/aws/ec2_node.py
+++ instance_control/aws/ec2_node.py
@@ -4,8 +4,8 @@ import copy
 import logging
 import time
 
+import boto3
 import yaml
-from boto3 import Session
 from botocore.config import Config
 
 from instance_control import config
@@ -17,45 +17,51 @@ _LOG = logging.getLogger('bubuku.cluster.aws.ec2_node')
 
 class EC2(object):
     def __init__(self, region, retries=100):
-        self.session = Session()
+        self.session = boto3.Session()
         self.retries = retries
         self.region = region
-        self._ec2 = None
+        boto3.set_stream_logger('boto3', logging.INFO)
+        self._client = None
         self._resource = None
 
     @property
     def client(self):
-        if not self._ec2:
-            self._ec2 = self.session.create_client(
+        if not self._client:
+            self._client = self.session.client(
                 'ec2',
-                region=self.region,
+                region_name=self.region,
                 config=Config(retries={'max_attempts': self.retries}))
-        return self._ec2
+        return self._client
 
     @property
     def resource(self):
         if not self._resource:
             self._resource = self.session.resource(
                 'ec2',
-                region=self.region,
+                region_name=self.region,
                 config=Config(retries={'max_attempts': self.retries}))
         return self._resource
 
     def _create_tagged_volume(self, cluster_config: dict, zone: str, name: str):
         _LOG.info('Creating EBS volume %s in %s', name, zone)
-        ebs_data = {
-            "AvailabilityZone": zone,
-            "VolumeType": cluster_config['volume_type'],
-            "Size": cluster_config['volume_size'],
-            "Encrypted": False, }
-        vol = self.client.create_volume(**ebs_data)
+        vol = self.client.create_volume(
+            AvailabilityZone=zone,
+            VolumeType=cluster_config['volume_type'],
+            Size=cluster_config['volume_size'],
+            Encrypted=False,
+        )
         _LOG.info('%s is successfully created', vol['VolumeId'])
 
         _LOG.info('Tagging %s with Taupage:erase-on-boot, to format only once', vol['VolumeId'])
-        tags = [{'Key': 'Name',
-                 'Value': name},
-                {'Key': 'Taupage:erase-on-boot',
-                 'Value': 'True'}]
+        tags = [
+            {
+                'Key': 'Name',
+                'Value': name
+            }, {
+                'Key': 'Taupage:erase-on-boot',
+                'Value': 'True'
+            }
+        ]
         self.client.create_tags(Resources=[vol['VolumeId']], Tags=tags)
 
     def _launch_instance(self, ip: str, subnet_: dict, ami: object, security_group_id: str, cluster_config: dict):
@@ -87,8 +93,10 @@ class EC2(object):
                 block_devices.append(bd)
             else:
                 # ignore any ephemeral volumes (aka. instance storage)
-                block_devices.append({'DeviceName': bd['DeviceName'],
-                                      'NoDevice': ''})
+                block_devices.append({
+                    'DeviceName': bd['DeviceName'],
+                    'NoDevice': ''
+                })
 
         if cluster_config['create_ebs']:
             self._create_tagged_volume(cluster_config, subnet_['AvailabilityZone'], config.KAFKA_LOGS_EBS)
@@ -111,36 +119,35 @@ class EC2(object):
             DisableApiTermination=False,
             EbsOptimized=True)
 
-        instance = resp['Instances'][0]
-        instance_id = instance['InstanceId']
+        instance_id = resp['Instances'][0]['InstanceId']
         _LOG.info('Instance %s launched, waiting for it to initialize', instance_id)
+        self.client.create_tags(
+            Resources=[instance_id],
+            Tags=[{'Key': 'Name', 'Value': cluster_config['cluster_name']}])
 
-        self.client.create_tags(Resources=[instance_id],
-                                Tags=[{'Key': 'Name', 'Value': cluster_config['cluster_name']}])
-
-        # wait for instance to initialize before we can assign a
-        # public IP address to it or tag the attached volume
-        while True:
-            resp = self.client.describe_instances(InstanceIds=[instance_id])
-            instance = resp['Reservations'][0]['Instances'][0]
-            if instance['State']['Name'] != 'pending':
-                _LOG.info('Instance %s is up', instance_id)
-                break
-            _LOG.info('Waiting 5 secs more ...')
-            time.sleep(5)
-
-        metric.create_auto_recovery_alarm(cluster_config, instance_id)
+        return instance_id
 
     def _launch_nodes(self, cluster_config: dict, node_ips: list):
+        starting_instances = []
         for subnet_, ip in node_ips:
-            self._launch_instance(
-                ip,
-                subnet_,
-                cluster_config['taupage_amis'],
-                security_group_id=cluster_config['security_group']['GroupId'],
-                cluster_config=cluster_config)
-            _LOG.info("Sleeping for one minute before launching next node..")
-            time.sleep(60)
+            starting_instances.append(
+                self._launch_instance(
+                    ip,
+                    subnet_,
+                    cluster_config['taupage_amis'],
+                    security_group_id=cluster_config['security_group']['GroupId'],
+                    cluster_config=cluster_config))
+        # wait for all instances to start
+        while starting_instances:
+            _LOG.info("Waiting for instances to start: {}".format(starting_instances))
+            time.sleep(5)
+            resp = self.client.describe_instances(InstanceIds=starting_instances)
+            started = [i['InstanceId'] for i in resp['Reservations']['Instances'] if i['State']['Name'] != 'pending']
+            if started:
+                _LOG.info('Instances {} started', started)
+            for instance_id in started:
+                starting_instances.remove(instance_id)
+                metric.create_auto_recovery_alarm(cluster_config, instance_id)
 
     def create(self, cluster_config: dict, instance_count: int):
         artifact_name = 'bubuku-appliance'
@@ -161,16 +168,16 @@ class EC2(object):
 
         except Exception as e:
             _LOG.error('''
-                You were trying to deploy Bubuku, but the process has failed :-(
-                
-                One of the reasons might be that some of Private IP addresses we were
-                going to use to launch the EC2 instances were taken by some other
-                instances in the middle of the process.  If that is the case, simply
-                retrying the operation might resolve the problem (you still might need
-                to clean up after this attempt before retrying).
-                
-                Please review the error message to see if that is the case, then
-                either correct the error or retry.
-                
-            ''')
+                    You were trying to deploy Bubuku, but the process has failed :-(
+                    
+                    One of the reasons might be that some of Private IP addresses we were
+                    going to use to launch the EC2 instances were taken by some other
+                    instances in the middle of the process.  If that is the case, simply
+                    retrying the operation might resolve the problem (you still might need
+                    to clean up after this attempt before retrying).
+                    
+                    Please review the error message to see if that is the case, then
+                    either correct the error or retry.
+                    
+                ''')
             raise e
