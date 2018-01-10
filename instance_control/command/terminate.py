4fb505f9f2e2675fe43c269c4cee6cd0256e52eb
Ricardo de Cillo <ricardo.de.cillo@zalando.de>
2018-01-10 14:50:20 +0100

    aruha-1456 avoid region inconsistency
    If the user executing the script has a default region different from the
    one specified by the cluster config, the script is aborted.
    
    This PR makes sure that the region used in piu is the same as the one
    configured in the cluster parameters.

--- instance_control/command/terminate.py
+++ instance_control/command/terminate.py
@@ -18,5 +18,5 @@ class TerminateCommand(Command):
     def execute(self):
         aws_ = AWSResources(region=self.cluster_config['region'])
         instance = node.get_instance_by_ip(aws_.ec2_resource, self.cluster_config['cluster_name'], self.ip)
-        piu.stop_taupage(self.ip, self.user, self.odd)
+        piu.stop_taupage(self.ip, self.user, self.odd, self.cluster_config['region'])
         node.terminate(aws_, self.cluster_config['cluster_name'], instance)
