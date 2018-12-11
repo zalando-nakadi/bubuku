1f1dd09ed05a07ba13a7f367cdef5f15ec05021b
Andrey Dyachkov <andrey.dyachkov@zalando.de>
2018-12-11 15:58:11 +0100

    updated version

--- bubuku/aws/cluster_config.py
+++ bubuku/aws/cluster_config.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 import yaml
 
@@ -30,7 +32,11 @@ class ClusterConfig():
         return 'eu-central-1'
 
     def get_instance_type(self):
-        return self._user_data.get('instance_type')
+        instance_type = self._user_data.get('instance_type')
+        if instance_type is None:
+            instance_type = os.getenv('INSTANCE_TYPE', 'm4.4xlarge')
+            self.set_instance_type(instance_type)
+        return instance_type
 
     def set_scalyr_account_key(self, scalyr_account_key):
         if scalyr_account_key:
@@ -79,7 +85,11 @@ class ClusterConfig():
         return self._user_data.get('volume_type')
 
     def get_vpc_id(self):
-        return self._user_data.get('vpc_id')
+        vpc_id = self._user_data.get('vpc_id')
+        if vpc_id is None:
+            vpc_id = os.getenv('VPC_ID')
+            self._user_data.get['vpc_id'] = vpc_id
+        return vpc_id
 
     def get_user_data(self):
         return dict(self._user_data)
