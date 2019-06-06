f40f2e806d06875837d4995105733e6d76748f7e
vstepanov <vyacheslav.stepanov@zalando.de>
2019-06-06 10:49:00 +0200

    ARUHA-2326: working on automatic cli help generation;

--- update_wiki.sh
+++ update_wiki.sh
@@ -1,3 +1,4 @@
+set -x #echo on
 cd ..
 git clone "https://github.com/zalando-nakadi/bubuku.wiki.git"
 cp -rf bubuku/cli_docs/cli.md bubuku.wiki/cli.md
@@ -6,3 +7,4 @@ git add .
 git commit -m "Generated Wiki via Travis-CI"
 git push
 cd ../bubuku
+set +x #echo off
